9b24b309a294ba7a404b7514c9f7cc4b82a6089d
Ivylucyawuor <awuorivy09>
2024-08-08 17:59:07 +0300

    FEtch

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -4,8 +4,59 @@ function ItemForm() {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  // Add function to handle submissions
+  function handleSubmit(e) {
+    e.preventDefault();
+    const itemData = {
+    name: name,
+    category: category,
+    isInCart: false,
+  };
+  console.log(itemData)
+
+  fetch("http://localhost:4000/items", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(itemData),
+  })
+    .then((r) => r.json())
+    .then((newItem) => onAddItem(newItem));
+}
+function Item({ item }) {
+  // Add function to handle button click
+  function handleAddToCartClick() {
+    console.log("clicked item:", item);
+  }
+
   return (
-    <form className="NewItem">
+    <li className={item.isInCart ? "in-cart" : ""}>
+      <span>{item.name}</span>
+      <span className="category">{item.category}</span>
+      {/* add the onClick listener */}
+      <button
+        className={item.isInCart ? "remove" : "add"}
+        onClick={handleAddToCartClick}
+      >
+        {item.isInCart ? "Remove From" : "Add to"} Cart
+      </button>
+      <button className="remove">Delete</button>
+    </li>
+  );
+}
+// Deconstruct the onDeleteItem prop
+function Item({ item, onUpdateItem, onDeleteItem }) {
+  function handleDeleteClick() {
+  fetch(`http://localhost:4000/items/${item.id}`, {
+    method: "DELETE",
+  })
+    .then((r) => r.json())
+    .then(() => console.log("deleted!"));
+}
+
+  return (
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -7,6 +7,30 @@ function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
+
+  // Add useEffect hook
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
+
+  // add this callback function
+  function handleDeleteItem(deletedItem) {
+    //console.log("In ShoppingCart:", deletedItem);
+    //function handleDeleteItem(deletedItem) {
+      const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+      setItems(updatedItems);
+    }
+  }
+   // add this function!
+   function handleAddItem(newItem) {
+    console.log("In ShoppingList:", newItem);
+  }
+
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
